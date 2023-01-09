bf6bbdfb16eb05167ea76aa5029fe3fa140078b2
Harry Fox <49591810+hfox1@users.noreply.github.com>
2023-01-09 20:42:17 +0000

    port 8080, app updated

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.json({ message: 'Hello!' });
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+	console.log(`Express app listening on ${PORT}`);
+});
