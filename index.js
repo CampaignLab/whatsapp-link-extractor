d4b0ac054271d60dd1f952cde434e060b64ea730
Harry Fox <49591810+hfox1@users.noreply.github.com>
2023-01-09 20:38:08 +0000

    project setup

---
+++ index.js
@@ -0,0 +1,12 @@
+const fileInput = document.querySelector('input[type="file"]');
+
+fileInput.addEventListener('change', () => {
+	const file = fileInput.files[0];
+	const reader = new FileReader();
+
+	reader.addEventListener('load', () => {
+		console.log(reader.result);
+	});
+
+	reader.readAsText(file);
+});
