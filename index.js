a6dc21d575051ccd268b4f35d062ebf91de041b8
Curtis Turk <curtis.c.turk@outlook.com>
2023-01-09 21:27:52 +0000

    Added file upload elements to the page

--- index.js
+++ index.js
@@ -5,8 +5,40 @@ fileInput.addEventListener("change", () => {
   const reader = new FileReader();
 
   reader.addEventListener("load", () => {
-    console.log(reader.result);
+    let data = reader.result.split("\n");
+    data.forEach((message, i) => {
+      let newElement = document.createElement("li");
+      // `message-${i}`
+      newElement.textContent = message;
+
+      let fullChat = document.getElementById("full-chat");
+      fullChat.appendChild(newElement);
+    });
+
+    // document.getElementById("full-chat").textContent = reader.result;
   });
 
   reader.readAsText(file);
+});
+
+const dropArea = document.defaultView;
+
+// Listen for file drop
+dropArea.addEventListener("drop", (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+
+  // Get the file
+  const file = event.dataTransfer.files[0];
+  console.log(file);
+
+  // Do something with the file
+});
+
+// Prevent default drag and drop behavior
+["dragenter", "dragover", "dragleave", "drop"].forEach((eventName) => {
+  dropArea.addEventListener(eventName, (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+  });
 });
