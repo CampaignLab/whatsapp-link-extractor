469843b1014945b487057f879c728ce1b85c262e
Curtis Turk <curtis.c.turk@outlook.com>
2023-01-10 12:36:34 +0000

    printing out links rather than full chat

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
+// File input field
 const fileInput = document.querySelector('input[type="file"]');
-
 fileInput.addEventListener("change", () => {
   const file = fileInput.files[0];
   printFileElements(file);
 });
 
+// File Drop
 const dropArea = document.defaultView;
-
-// Listen for file drop
 dropArea.addEventListener("drop", (event) => {
   event.preventDefault();
   event.stopPropagation();
   const file = event.dataTransfer.files[0];
   printFileElements(file);
 });
-
 // Prevent default drag and drop behavior
 ["dragenter", "dragover", "dragleave", "drop"].forEach((eventName) => {
   dropArea.addEventListener(eventName, (event) => {
@@ -28,8 +26,9 @@ const printFileElements = (file) => {
   reader.readAsText(file);
 
   reader.addEventListener("load", () => {
-    let data = reader.result.split("\n");
-    data.forEach((message, i) => {
+    // let data = reader.result.split("\n");
+    let fileLinks = extractLinks(reader.result);
+    fileLinks.forEach((message) => {
       let newElement = document.createElement("li");
       newElement.textContent = message;
 
@@ -38,3 +37,8 @@ const printFileElements = (file) => {
     });
   });
 };
+
+function extractLinks(text) {
+  var urlRegex = /(https?:\/\/[^\s]+)/g;
+  return text.match(urlRegex);
+}
