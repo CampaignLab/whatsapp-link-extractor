8169558fd5d0dcdd48b242bf9c3561733a4ed3bc
Curtis Turk <curtis.c.turk@outlook.com>
2023-01-10 12:06:57 +0000

    Refactored file upload functions into a single method

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-app.get('/', (req, res) => {
-	res.json({ message: 'Hello!' });
-});
+// app.get('/', (req, res) => {
+// 	res.json({ message: 'Hello!' });
+// });
 
-const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-	console.log(`Express app listening on ${PORT}`);
-});
+// app.listen(PORT, () => {
+// 	console.log(`Express app listening on ${PORT}`);
+// });
--- index.js
+++ index.js
@@ -2,23 +2,7 @@ const fileInput = document.querySelector('input[type="file"]');
 
 fileInput.addEventListener("change", () => {
   const file = fileInput.files[0];
-  const reader = new FileReader();
-
-  reader.addEventListener("load", () => {
-    let data = reader.result.split("\n");
-    data.forEach((message, i) => {
-      let newElement = document.createElement("li");
-      // `message-${i}`
-      newElement.textContent = message;
-
-      let fullChat = document.getElementById("full-chat");
-      fullChat.appendChild(newElement);
-    });
-
-    // document.getElementById("full-chat").textContent = reader.result;
-  });
-
-  reader.readAsText(file);
+  printFileElements(file);
 });
 
 const dropArea = document.defaultView;
@@ -27,25 +11,8 @@ const dropArea = document.defaultView;
 dropArea.addEventListener("drop", (event) => {
   event.preventDefault();
   event.stopPropagation();
-
-  // Get the file
   const file = event.dataTransfer.files[0];
-  console.log(file);
-
-  const reader = new FileReader();
-
-  reader.readAsText(file);
-  reader.addEventListener("load", () => {
-    let data = reader.result.split("\n");
-    data.forEach((message, i) => {
-      let newElement = document.createElement("li");
-      // `message-${i}`
-      newElement.textContent = message;
-
-      let fullChat = document.getElementById("full-chat");
-      fullChat.appendChild(newElement);
-    });
-  });
+  printFileElements(file);
 });
 
 // Prevent default drag and drop behavior
@@ -55,3 +22,19 @@ dropArea.addEventListener("drop", (event) => {
     event.stopPropagation();
   });
 });
+
+const printFileElements = (file) => {
+  const reader = new FileReader();
+  reader.readAsText(file);
+
+  reader.addEventListener("load", () => {
+    let data = reader.result.split("\n");
+    data.forEach((message, i) => {
+      let newElement = document.createElement("li");
+      newElement.textContent = message;
+
+      let fullChat = document.getElementById("full-chat");
+      fullChat.appendChild(newElement);
+    });
+  });
+};
